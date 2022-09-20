8b34641fc384fc9c5188a8dadcc7e3c343230073
Elshad <khanakhmedov.elshad@mail.ru>
2022-09-20 23:58:13 +0300

    first commit

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 6000
 
 app.use(express.json());
 
-app.use("/api", router)
+app.use("/ht_02/api", router)
 
 app.listen(PORT, () => {
     console.log(`Server started on port: ${PORT}`)
