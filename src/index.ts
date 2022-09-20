5f3bbeff7aea6e9952091f73c49e29bff350f68d
Elshad <khanakhmedov.elshad@mail.ru>
2022-09-21 00:27:22 +0300

    first commit

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import express from 'express'
-import {router} from "./routes/IndexRoutes";
+// import {router} from "./routes/IndexRoutes";
+import {blogRouter} from "./routes/blogRouter";
+import {postRouter} from "./routes/postRouter";
 
 const app = express()
 
@@ -7,7 +9,7 @@ const PORT = process.env.PORT || 6000
 
 app.use(express.json());
 
-app.use("/ht_02/api", router)
+app.use("/ht_02/api", blogRouter, postRouter)
 
 app.listen(PORT, () => {
     console.log(`Server started on port: ${PORT}`)
